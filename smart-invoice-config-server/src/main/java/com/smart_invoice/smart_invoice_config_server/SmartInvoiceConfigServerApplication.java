3381d1011e6f42f9aee97375202260bd2241f57d
Alessio <ale.pizzi@outlook.com>
2025-04-30 10:49:29 +0200

    create config server

--- smart-invoice-config-server/src/main/java/com/smart_invoice/smart_invoice_config_server/SmartInvoiceConfigServerApplication.java
+++ smart-invoice-config-server/src/main/java/com/smart_invoice/smart_invoice_config_server/SmartInvoiceConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.smart_invoice.smart_invoice_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SmartInvoiceConfigServerApplication {
 
 	public static void main(String[] args) {
