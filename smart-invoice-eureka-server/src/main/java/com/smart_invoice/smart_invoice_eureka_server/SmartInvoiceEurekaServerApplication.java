27d66c83f908faf95ad4c11e9bb55245078787b7
Alessio <ale.pizzi@outlook.com>
2025-04-30 10:54:32 +0200

    create eureka server

--- smart-invoice-eureka-server/src/main/java/com/smart_invoice/smart_invoice_eureka_server/SmartInvoiceEurekaServerApplication.java
+++ smart-invoice-eureka-server/src/main/java/com/smart_invoice/smart_invoice_eureka_server/SmartInvoiceEurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.smart_invoice.smart_invoice_eureka_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SmartInvoiceEurekaServerApplication {
 
 	public static void main(String[] args) {
